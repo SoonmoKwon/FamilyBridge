91be78bcaa4aa2f8e8e7ca45b073c2efd01b6edb
soonmo Kwon <asifiloveu@postech.edu>
2013-07-22 16:16:38 +0900

    tested basic access to the views and expense app

--- expense/tests.py
+++ expense/tests.py
@@ -6,11 +6,34 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test.client import Client
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
+
+    def runTest(self):
+
+        pass
+
+    def setUp(self):
+        self.client = Client()
+
+
+    def test_loading_views(self):
         """
-        Tests that 1 + 1 always equals 2.
+            We are only testing GET request to views
         """
-        self.assertEqual(1 + 1, 2)
+
+        self.client.get(reverse('expense_home'))
+        self.client.get(reverse('expense_add'))
+        self.client.get(reverse('expense_item_add'))
+        self.client.get(reverse('expense_download'))
+        self.client.get(reverse('expense_category_name_sort', kwargs= {'category_name': 'food'}))
+        self.client.get(reverse('expense_category_id_sort', kwargs= {'category_id': '123'}))
+        self.client.get(reverse('expense_teammate_name_sort', kwargs={'teammate_name': 'food'}))
+        self.client.get(reverse('expense_teammate_id_sort', kwargs={'teammate_id': '123'}))
+        self.client.get(reverse('expense_category_name_filter', kwargs={'category_name': 'food'}))
+        self.client.get(reverse('expense_category_id_filter', kwargs={'category_id': '123'}))
+        self.client.get(reverse('expense_teammate_name_filter', kwargs={'teammate_name': 'food'}))
+        self.client.get(reverse('expense_teammate_id_filter', kwargs={'teammate_id': '123'}))
--- expense/views.py
+++ expense/views.py
@@ -21,34 +21,34 @@ def download(request):
     data = {}
     return HttpResponseRedirect(reverse('expense_home'))
 
-def sort_by_category_name(request):
+def sort_by_category_name(request, category_name):
     data = {}
     return render(request, 'expense/home.html', data)
     
-def sort_by_category_id(request):
+def sort_by_category_id(request, category_id):
     data = {}
     return render(request, 'expense/home.html', data)    
 
-def sort_by_teammate_name(request):
+def sort_by_teammate_name(request, teammate_name):
     data = {}
     return render(request, 'expense/home.html', data)
                   
-def sort_by_teammate_id(request):
+def sort_by_teammate_id(request, teammate_id):
     data = {}
     return render(request, 'expense/home.html', data)    
 
-def filter_by_category_name(request):
+def filter_by_category_name(request, category_name):
     data = {}
     return render(request, 'expense/home.html', data)
     
-def filter_by_category_id(request):
+def filter_by_category_id(request, category_id):
     data = {}
     return render(request, 'expense/home.html', data)    
 
-def filter_by_teammate_name(request):
+def filter_by_teammate_name(request, teammate_name):
     data = {}
     return render(request, 'expense/home.html', data)
                   
-def filter_by_teammate_id(request):
+def filter_by_teammate_id(request, teammate_id):
     data = {}
-    return render(request, 'expense/home.html', data)  
+    return render(request, 'expense/home.html', data)    
