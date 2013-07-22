87ad1aab6d766b6a60ddc33794da90969f042987
soonmo Kwon <asifiloveu@postech.edu>
2013-07-22 15:33:35 +0900

    started the expense application

--- FamilyBridge/urls.py
+++ FamilyBridge/urls.py
@@ -15,4 +15,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^calendar/', include('fbcalendar.urls')),
+    url(r'^expense/', include('expense.urls')),
 )
---
+++ expense/views.py
@@ -0,0 +1,54 @@
+# Create your views here.
+
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+
+def home(request):
+    data = {}
+    return render(request, 'expense/home.html', data)
+
+def add(request):
+    data = {}
+    return render(request, 'expense/add.html', data)
+
+def item_add(request):
+    data = {}
+    return HttpResponseRedirect(reverse('expense_home'))
+
+def download(request):
+    data = {}
+    return HttpResponseRedirect(reverse('expense_home'))
+
+def sort_by_category_name(request):
+    data = {}
+    return render(request, 'expense/home.html', data)
+    
+def sort_by_category_id(request):
+    data = {}
+    return render(request, 'expense/home.html', data)    
+
+def sort_by_teammate_name(request):
+    data = {}
+    return render(request, 'expense/home.html', data)
+                  
+def sort_by_teammate_id(request):
+    data = {}
+    return render(request, 'expense/home.html', data)    
+
+def filter_by_category_name(request):
+    data = {}
+    return render(request, 'expense/home.html', data)
+    
+def filter_by_category_id(request):
+    data = {}
+    return render(request, 'expense/home.html', data)    
+
+def filter_by_teammate_name(request):
+    data = {}
+    return render(request, 'expense/home.html', data)
+                  
+def filter_by_teammate_id(request):
+    data = {}
+    return render(request, 'expense/home.html', data)  
