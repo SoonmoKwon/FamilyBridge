6a340f7a6169a8425193a5ce5398ba0d12234b20
Woo-hyeon Shim <dreams90@postech.ac.kr>
2013-07-22 16:43:03 +0900

    added models for calendar

--- fbcalendar/models.py
+++ fbcalendar/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from core.models import Category
 
-# Create your models here.
+class Activity (models.Model):
+    team_member = models.ForeignKey(User)
+    category = models.ForeignKey(Category)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    creation_time = models.DateTimeField(auto_now_add = True)
+    update_time = models.DateTimeField(auto_now = True)
