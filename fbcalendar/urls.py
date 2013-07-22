5239eaedd82ba06e3e2534b7345c97f19953f4d2
Woo-hyeon Shim <dreams90@postech.ac.kr>
2013-07-22 15:43:25 +0900

    fbcalendar urls changed

--- fbcalendar/urls.py
+++ fbcalendar/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'fbcalendar.views.home', name='calendar_home'),
-    url(r'^/add/$', 'fbcalendar.views.add', name='calendar_add'),
-    url(r'^/activity/add/$', 'fbcalendar.views.activity_add', name='calendar_activity_add'),
-    url(r'^/download/$', 'fbcalendar.views.download', name='calendar_download'),
-    url(r'^/(?P<day>\d{2}\d{2}\d{4})/$', 'fbcalendar.views.dayplan', name='calendar_dayplan'),
-    url(r'^/week/(?P<day>\d{2}\d{2}\d{4})/$', 'fbcalendar.views.weekplan', name='calendar_weekplan'),
+    url(r'^add/$', 'fbcalendar.views.add', name='calendar_add'),
+    url(r'^activity/add/$', 'fbcalendar.views.activity_add', name='calendar_activity_add'),
+    url(r'^download/$', 'fbcalendar.views.download', name='calendar_download'),
+    url(r'^(?P<day>\d{2}\d{2}\d{4})/$', 'fbcalendar.views.dayplan', name='calendar_dayplan'),
+    url(r'^week/(?P<day>\d{2}\d{2}\d{4})/$', 'fbcalendar.views.weekplan', name='calendar_weekplan'),
 )
