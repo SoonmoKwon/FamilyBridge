6a5d000c1ada40fad64657566b50b641519924e3
leehc <leehc@postech.ac.kr>
2013-07-24 15:03:43 +0900

    template for homepage

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,7 @@
-from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django import forms
+from django.forms import ModelForm
+
 
 class UserForm(ModelForm):
     class Meta:
